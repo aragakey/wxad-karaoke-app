a07ff13bf72da03b838dfd5848c321195ad9286b
yijiejiang <yijiejiang@tencent.com>
2025-10-28 17:58:14 +0800

    chore

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client', '@prisma/engines'],
-  },
+  serverExternalPackages: ['@prisma/client', '@prisma/engines'],
   outputFileTracingIncludes: {
     '/api/**/*': ['./node_modules/.prisma/client/**/*'],
   },
